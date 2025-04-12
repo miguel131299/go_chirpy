e0d95b5006b8e07a1b650b8371db1f1b14d82975
Miguel Marcano <miguel131299@gmail.com>
2025-04-12 15:34:50 +0200

    add ch1 l11

--- main.go
+++ main.go
@@ -4,9 +4,18 @@ import (
 	"net/http"
 )
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	// Strip "/app" from URL path before passing to FileServer
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
