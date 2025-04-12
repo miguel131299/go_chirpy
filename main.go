9bdbd968533c24d281290a57a59cacb09d9fbf6d
Miguel Marcano <miguel131299@gmail.com>
2025-04-12 15:11:08 +0200

    add ch1 l4

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("hello world")
+
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+
+}
